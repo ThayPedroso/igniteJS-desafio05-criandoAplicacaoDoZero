2f728b6b9f677452f96618beecdbd7ddfac36b24
Thayla Pedroso <thayla.pedroso88@gmail.com>
2022-07-06 07:14:50 -0300

    document structure. Global and home CSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next';
 
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,9 +25,46 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <main className={styles.container}>
+      <header>
+        <img src="/images/Logo.svg" alt="logo" />
+      </header>
+      <div className={styles.posts}>
+        <a>
+          <strong>Como utilizar Hooks</strong>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div>
+            <div className={styles.updatedAt}>
+              <FiCalendar />
+              <time>15 Mar 2021</time>
+            </div>
+            <div className={styles.author}>
+              <FiUser />
+              <span>Thayla Pedroso</span>
+            </div>
+          </div>
+        </a>
+        <a>
+          <strong>Como utilizar Hooks</strong>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div>
+            <div className={styles.updatedAt}>
+              <FiCalendar />
+              <time>15 Mar 2021</time>
+            </div>
+            <div className={styles.author}>
+              <FiUser />
+              <span>Thayla Pedroso</span>
+            </div>
+          </div>
+        </a>
+        <button type="button">Carregar mais posts</button>
+      </div>
+    </main>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
